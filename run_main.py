b2bf0afffbbcbabf3288e3216104f978eb0f490f
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2024-10-08 16:48:06 +0300

    Create run_main.py
    run file for main.py

---
+++ run_main.py
@@ -0,0 +1,13 @@
+from main import Meteomatics_Service
+
+def run():
+    
+    meteomatics=Meteomatics_Service()
+
+    print("Running...")
+    meteomatics.run()
+
+
+if __name__ == "__main__":
+    run()
+
